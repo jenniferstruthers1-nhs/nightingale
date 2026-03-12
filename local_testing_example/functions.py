d956ffa8c1ed1e553d0915293b9a80a9927b7690
Jennifer Struthers <jennifer.struthers1@nhs.net>
2026-03-12 11:01:41 +0000

    add assertion err

--- local_testing_example/functions.py
+++ local_testing_example/functions.py
@@ -8,5 +8,7 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    # Fixed: Changed 9/5 to 5/9
+    if fahrenheit < -459.67:
+        raise ValueError("Temperature below absolute zero is physically impossible!")
+        
     return multiply(subtract(fahrenheit, 32), 5 / 9)
